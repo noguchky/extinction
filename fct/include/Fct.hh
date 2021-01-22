b05838f456dda0414cf5dbad85ec43680f69277a
extinction <extinction@comet-eb01>
2021-01-22 21:09:18 +0900

    add decoder function

--- fct/include/Fct.hh
+++ fct/include/Fct.hh
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include "TTree.h"
 #include "Units.hh"
 #include "Tdc.hh"
@@ -14,6 +15,7 @@ namespace Extinction {
   namespace Fct {
 
     const std::string Name = "FCT";
+    const std::size_t NofChannels = 32;
 
     namespace ChannelMap {
       // (*) key should be board no & raw channel
@@ -63,36 +65,184 @@ namespace Extinction {
          { 14, 0 },
         };
     }
- 
+
+    struct DataType {
+      enum {
+            None,
+            Header,
+            GateStart,
+            GateEnd,
+            Carry, // ~ Heartbeat/Overflow
+            Data,
+            Error,
+      };
+    };
+    
+    // 4 byte
+    using Packet_t = UInt_t;
+
+    inline void ShowAsHex(Packet_t data) {
+      printf("%08x \n", data);
+    }
+
+    inline Bool_t IsHeader(Packet_t data) {
+      // return (data & 0xffffffff) == 0x12345678;
+      return data == 0x12345678;
+    }
+    inline Bool_t IsGateStart(Packet_t data) {
+      // return (data & 0xff0000ff) == 0xff0000aa;
+      return data  == 0xffffaaaa;
+    }
+    inline Bool_t IsGateEnd(Packet_t data) {
+      return (data & 0xff0000ff) == 0xff000055;
+    }
+    inline Bool_t IsCarry(Packet_t data) {
+      return (data & 0xffff0000) == 0xffff0000;
+    }
+    inline Bool_t IsData(Packet_t data) {
+      return
+        ((data & 0xf0000000) == 0xc0000000) ||
+        ((data & 0xf0000000) == 0xd0000000);
+    }
+
+    inline Int_t GetCarry(Packet_t data) {
+      return data & 0xff;
+    }
+    inline Int_t GetChannel(Packet_t data) {
+      return (data >> 24) & 0x1f;
+    }
+    inline Int_t GetTdc(Packet_t data) {
+      return data & 0x00ffffff;
+    }
+    
     class FctData : public ITdcDataProvider {
     public:
+      Int_t Type;
       Int_t Spill;
       Int_t Channel;
       Int_t DTime;   // ? ... currently, all data are filled with 0
       Int_t Tdc;
+      Int_t Carry;
+
+      Int_t  PreviousCarry[NofChannels];
+      UInt_t PreviousTdc  [NofChannels];
 
       FctData() {
         Clear();
       }
       FctData(const FctData& data)
-        : Spill    (data.Spill    ),
+        : Type     (data.Type     ),
+          Spill    (data.Spill    ),
           Channel  (data.Channel  ),
           DTime    (data.DTime    ),
-          Tdc      (data.Tdc      ) {
+          Tdc      (data.Tdc      ),
+          Carry    (data.Carry    ) {
+        for (std::size_t ch = 0; ch < NofChannels; ++ch) {
+          PreviousCarry[ch] = data.PreviousCarry[ch];
+          PreviousTdc  [ch] = data.PreviousTdc  [ch];
+        }
       }
       FctData& operator=(const FctData& data) {
+        Type      = data.Type;
         Spill     = data.Spill;
         Channel   = data.Channel;
         DTime     = data.DTime;
         Tdc       = data.Tdc;
+        Carry     = data.Carry;
+        for (std::size_t ch = 0; ch < NofChannels; ++ch) {
+          PreviousCarry[ch] = data.PreviousCarry[ch];
+          PreviousTdc  [ch] = data.PreviousTdc  [ch];
+        }
         return *this;
       }
 
       inline void Clear() {
+        Type      = DataType::None;
         Spill     = -1;
         Channel   = 0;
         DTime     = 0;
         Tdc       = 0;
+        Carry     = 0;
+        for (std::size_t ch = 0; ch < NofChannels; ++ch) {
+          PreviousCarry[ch] = 0;
+          PreviousTdc  [ch] = 0;
+        }
+      }
+
+      inline void SetDataAsHeader(Packet_t) {
+        Type = DataType::Header;
+      }
+
+      inline void SetDataAsGateStart(Packet_t) {
+        Type = DataType::GateStart;
+        ++Spill;
+        Channel = 0;
+        DTime   = 0;
+        Tdc     = 0;
+        Carry   = 0;
+        for (std::size_t ch = 0; ch < NofChannels; ++ch) {
+          PreviousCarry[ch] = 0;
+          PreviousTdc  [ch] = 0;
+        }
+      }
+
+      inline void SetDataAsGateEnd(Packet_t) {
+        Type = DataType::GateEnd;
+      }
+
+      inline void SetDataAsCarry(Packet_t data) {
+        Type  = DataType::Carry;
+        Carry = GetCarry(data);
+        for (std::size_t ch = 0; ch < NofChannels; ++ch) {
+          PreviousCarry[ch] = Carry - 1;
+        }
+      }
+
+      inline void SetDataAsData(Packet_t data) {
+        Type      = DataType::Data;
+        Channel   = GetChannel(data);
+        Int_t tdc = GetTdc(data);
+
+        if (Channel < 0 || (Long64_t)NofChannels <= Channel) {
+          Type = DataType::Error;
+          return;
+        }
+
+        Tdc = tdc + PreviousCarry[Channel] * 0x1000000;
+        if ((Long64_t)Tdc < PreviousTdc[Channel]) {
+          PreviousCarry[Channel] = Carry;
+          Tdc = tdc + PreviousCarry[Channel] * 0x1000000;
+        }
+        PreviousTdc[Channel] = Tdc;
+      }
+
+      std::basic_istream<char>& Read(std::basic_istream<char>& file,
+                                     Packet_t* packet = nullptr) {
+        Packet_t buff;
+        auto& ret = file.read((char*)&buff, sizeof(Packet_t));
+        if (!ret) {
+          return ret;
+        }
+
+        if        (IsHeader(buff)) {
+          SetDataAsHeader(buff);
+        } else if (IsGateStart(buff)) {
+          SetDataAsGateStart(buff);
+        } else if (IsGateEnd(buff)) {
+          SetDataAsGateEnd(buff);
+        } else if (IsCarry(buff)) {
+          SetDataAsCarry(buff);
+        } else if (Fct::IsData(buff)) {
+          SetDataAsData(buff);
+        } else {
+          Type = DataType::Error;
+        }
+
+        if (packet) {
+          *packet = buff;
+        }
+
+        return ret;
       }
 
       inline virtual Double_t GetTimePerTdc() const override {
@@ -113,6 +263,7 @@ namespace Extinction {
 
       inline virtual void SetBranchAddress(TTree* tree) override {
         // std::cout << "Fct::FctData::SetBranchAddress()" << std::endl;
+        Type = DataType::Data;
         tree->SetBranchAddress("CH"   , &Channel);
         tree->SetBranchAddress("TDC"  , &Tdc    );
         tree->SetBranchAddress("DTIME", &DTime  );
@@ -131,7 +282,7 @@ namespace Extinction {
       }
 
       inline virtual Bool_t IsData() const override {
-        return true;
+        return Type == DataType::Data;
       }
 
       inline virtual Int_t GetSpill() const override {
@@ -162,6 +313,56 @@ namespace Extinction {
 
     };
 
+    class Decoder {
+    public:
+      FctData Data;
+      TTree*  Tree;
+
+      Decoder() : Tree(nullptr) {
+      }
+
+      void InitializeTree(const std::string& treename = "FCTTDC") {
+        // std::cout << "Fct::Decoder::InitializeTree()" << std::endl;
+        if (Tree) {
+          delete Tree;
+        }
+        Tree = new TTree(treename.data(), treename.data());
+
+        Data.CreateBranch(Tree);
+      }
+
+      std::size_t Decode(std::basic_istream<char>& file) {
+        // std::cout << "Fct::Decoder::Decode()" << std::endl;
+        std::size_t count = 0UL;
+        if (!Tree) {
+          std::cout << "[warning] tree has not initialized yet" << std::endl;
+        } else {
+          for (; Read(file); ++count) {
+            // Data.Show();
+            if (Data.Type == DataType::Data) {
+              // std::cout << "TTree::Fill()" << std::endl;
+              Tree->Fill();
+            }
+          }
+        }
+        return count;
+      }
+
+      std::size_t Decode(const std::string& filename) {
+        std::ifstream file(filename);
+        if (!file) {
+          std::cerr << "[error] file is not opened, " << filename << std::endl;
+          return 0UL;
+        }
+        return Decode(file);
+      }
+
+      inline std::basic_istream<char>& Read(std::basic_istream<char>& file,
+                                            Packet_t* packet = nullptr) {
+        return Data.Read(file, packet);
+      }
+    };
+    
   }
 
 }
